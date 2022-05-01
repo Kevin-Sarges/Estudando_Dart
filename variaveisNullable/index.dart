0b7fe0bf4cb3cf0b96c5e824700bf4dc989d9ddf
Kevin-Sarges <kevinsarges08@gmail.com>
2022-05-01 20:32:42 -0300

    init

---
+++ variaveisNullable/index.dart
@@ -0,0 +1,18 @@
+void main() {
+  saudacoes('Kevin');
+}
+
+void saudacoes(String nome, {bool mostrarAgora = true}) {
+  print('Saudações do $nome');
+  print('Seja bem-vindo(a)! ');
+
+  if (mostrarAgora) {
+    print('Agora: ${agora()}');
+  }
+}
+
+String agora() {
+  DateTime agora = DateTime.now();
+
+  return agora.toString();
+}
